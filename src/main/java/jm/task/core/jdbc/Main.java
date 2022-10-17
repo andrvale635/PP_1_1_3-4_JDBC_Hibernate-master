41e348f70cc14839c32aacba31966bf95eac831b
andrvale635 <89186098454valiok>
2022-10-17 15:21:52 +0300

    V2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,36 +8,37 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        UserServiceImpl usi = new UserServiceImpl();
+
         ////////////////////////  OK  //////////////////////
-        /*System.out.println("Создание таблицы");
+        System.out.println("Создание таблицы");
         usi.createUsersTable();
-        System.out.println();*/
+        System.out.println();
 
         ////////////////////////  OK  //////////////////////
-        /*for (int i = 0; i<4; i++){
+        for (int i = 0; i<4; i++){
             usi.saveUser("New", "Test", (byte) 20);
-        }*/
+        }
 
         ////////////////////////  OK  //////////////////////
-        /*System.out.println("Получение всех данных из таблицы");
+        System.out.println("Получение всех данных из таблицы");
         usi.getAllUsers();
-        System.out.println();*/
+        System.out.println();
 
         ////////////////////////  OK  //////////////////////
-        /*System.out.println("удаление user с id = n");
-        usi.removeUserById(9);
+        System.out.println("удаление user с id = n");
+        usi.removeUserById(4);
         usi.getAllUsers();
-        System.out.println();*/
+        System.out.println();
 
         ////////////////////////  OK  //////////////////////
-       /* System.out.println("Отчистка таблицы");
+        System.out.println("Отчистка таблицы");
         usi.cleanUsersTable();
-        usi.getAllUsers();*/
+        usi.getAllUsers();
 
         ////////////////////////  OK  //////////////////////
-        /*System.out.println("Удаление таблицы");
+        System.out.println("Удаление таблицы");
         usi.dropUsersTable();
-        System.out.println();*/
+        System.out.println();
     }
+    private static final UserServiceImpl usi = new UserServiceImpl();
 }
